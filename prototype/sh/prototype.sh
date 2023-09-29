53160bbd6c0d7ed0419e63f6780ba8773081acb4
N3v1 <n3v1010@gmail.com>
2023-09-29 21:04:04 +0200

    Update prototype.sh

--- prototype/sh/prototype.sh
+++ prototype/sh/prototype.sh
@@ -19,11 +19,11 @@ case "$option" in
         ;;
     i)
         # Install the CM Command Line Tools
-        exit 0
-        ;;
+        echo "Installation started ..."
+        xcode-select --install && xcode-select --version && exit 0 ;;
     x)
         # Install the Xcode Command Line Tools
-        xcode-select --install && exit 0 ;;
+        echo "Installation started ..." && xcode-select --install && exit 0 ;;
     c)
         # Install CodeMaster (command not available yet)
         exit 0
